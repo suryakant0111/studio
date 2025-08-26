dd926c816aaca548d6550e7768647ec3e6734f5f
Suryakant Khevji <suryakantvkhe@gmail.com>
2025-08-26 23:29:33 +0530

    auth test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,16 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBNDfwcDnUaJTPBDnR6hz67tpvUUn7AG2I",
-  authDomain: process.env.NODE_ENV === 'development' 
-    ? 'localhost' 
-    : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "nutrichef-6a8sc.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "nutrichef-6a8sc",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "nutrichef-6a8sc.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "824487196976",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:824487196976:web:8f4d44461e9b779163b0f5",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NODE_ENV === 'development'
+    ? 'localhost'
+    : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || undefined,
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();

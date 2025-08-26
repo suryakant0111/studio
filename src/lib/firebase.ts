f05b77ccf7a884426174cf262fbce3c380ca16e8
Suryakant Khevji <suryakantvkhe@gmail.com>
2025-08-27 00:33:11 +0530

    test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const isLocalhost = isClient &&
    window.location.hostname === '127.0.0.1');
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBNDfwcDnUaJ...",
   authDomain: isLocalhost 
     ? 'localhost' 
     : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "nutri-chef-seven.vercel.app",

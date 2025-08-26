0e5660ec9211daa290b0805b5e2494816bd08a41
Suryakant Khevji <suryakantvkhe@gmail.com>
2025-08-26 23:36:40 +0530

    Authtesting

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,9 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NODE_ENV === 'development'
-    ? 'localhost'
-    : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "nutri-chef-seven.vercel.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
